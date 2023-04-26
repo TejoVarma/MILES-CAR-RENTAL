33dbe1926992aa7aae5ae68c5afb2e13363c6d4c
TejoVarma <tejovarma.alluri9@gmail.com>
2023-04-26 16:01:18 +0530

    Added Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className='main'>
+      <Header/>
+      <div>
+        Hello World
+      </div>
     </div>
   );
 }
