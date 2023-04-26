d4c0e243c24dc9b6314a5ece59f3b76992262c21
TejoVarma <tejovarma.alluri9@gmail.com>
2023-04-26 16:47:40 +0530

    Added Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='main'>
       <Header/>
-      <div>
+      <div className='body'>
         Hello World
       </div>
+      <Footer/>
     </div>
   );
 }
