ad587b0f68b78df9be0bdbd11192d391877bdf95
TejoVarma <tejovarma.alluri9@gmail.com>
2023-04-27 13:14:12 +0530

    Added the testimonals and services section in landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -9,23 +9,8 @@ export default function LandingPage() {
                     <div className="paragraph">
                         <p>Miles Car Rental is the go-to choice for car rental needs in your place. We offer a wide selection of cars for hire, from economy to luxury, with competitive rates and top-notch customer service. Whether you need a car for a day, a week or longer, Miles has you covered. <br /> <span className="easy">Book with us for an easy and stress-free car rental experience.</span></p>
                     </div>
-                    <div className="user-login-links">
-                        <div className="user-links-section">
-                            <p className="existing-user">If you are a member of Miles family,</p>
-                            <button className="user-login">LogIn</button>
-                            <p className="new-user">If you are a new user then join us...</p>
-                            <button className="user-signup">SignUp</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="admin-section">
-                    <div className="admin-login-links">
-                        <div className="admin-links-section">
-                            <p className="existing-admin">If you are a collaborator of Miles family,</p>
-                            <button className="admin-login">Host LogIn</button>
-                            <p className="new-admin">If you have a car and want to grow with us...then collab with us</p>
-                            <button className="admin-signup">Host SignUp</button>
-                        </div>
+                    <div className="landing-button-container">
+                        <button className="landing-button">Get Started....</button>
                     </div>
                 </div>
             </div>
@@ -38,18 +23,33 @@ export default function LandingPage() {
         </div>
         <div className="services">
             <div className="service">
-                <div>
-                    <img src="./service1.jpeg" alt="service1"/>
+                <div className="service-image-container">
+                    <img className="service-image" src="./service1.jpeg" alt="service1" />
                 </div>
-                <div>
-                    <p></p>
+                <div className="service-description">
+                    <p className="service-heading">One Way Rental</p>
+                    <p className="service-para">Allows customers to pick up a vehicle at one location and drop it off at another.</p>
                 </div>
             </div>
             <div className="service">
-                service2
+                <div className="service-description">
+                    <p className="service-heading">Long Term Rental</p>
+                    <p className="service-para">Offers longer term car rentals, typically for 30 days or more.</p>
+                </div>
+                <div className="service-image-container">
+                    <img className="service-image" src="./service2.jpeg" alt="service1" />
+                </div>
             </div>
             <div className="service">
-                service3
+                <div className="service">
+                    <div className="service-image-container">
+                        <img className="service-image" src="./service3.jpeg" alt="service1" />
+                    </div>
+                    <div className="service-description">
+                        <p className="service-heading">Luxury Rental</p>
+                        <p className="service-para">Provides customers with access to luxury vehicles, such as high end SUVs and sports cars.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
