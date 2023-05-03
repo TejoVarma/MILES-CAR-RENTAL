ac1191838c4aee41a6d435fbe176fd03005a7dcf
TejoVarma <tejovarma.alluri9@gmail.com>
2023-05-03 16:39:26 +0530

    Added a link tag to navigate to login page

---
+++ client/src/components/login/Login.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Login(){
+    return <h1>I am from Login Page</h1>
+}
--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "../components/LandingPage";
+import Login from "../components/login/Login";
 
 export default function Routers() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login/>}/>
         </Routes>
     </BrowserRouter>
 }
